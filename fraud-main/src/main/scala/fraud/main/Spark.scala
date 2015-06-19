aa494cdba9abed2f1a8d4689986ae934f33920eb
Borys Biletskyy <borys.biletskyy@gmail.com>
2015-06-19 12:05:15 +0200

    Receiver Actor has been added

--- fraud-main/src/main/scala/fraud/main/Spark.scala
+++ fraud-main/src/main/scala/fraud/main/Spark.scala
@@ -1,7 +1,24 @@
 package fraud.main
 
+import org.apache.spark.streaming.receiver.ActorHelper
+
+import akka.actor.Actor
+
+
+/** Object in charge of running stream analytics */
 object Spark {
   def main(args: Array[String]): Unit = {
     println("Hello Spark")
+  }
+}
+
+/** This actor is a bridge to Spark. It receives transactions and puts them to the spark stream */
+class Receiver extends Actor with ActorHelper {
+  override def preStart() = {
+    println(s"Starting Spark Transaction Receiver actor at ${context.self.path}")
+  }
+  def receive = {
+    case t: Transaction =>
+      store(t)
   }
 }
