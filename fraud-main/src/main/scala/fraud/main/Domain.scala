86bacf8f45f0d91203964755c9e04cb078579b4f
Anton Sergienko <antons@backbase.com>
2015-06-19 12:18:19 +0200

    added domain stuff

--- fraud-main/src/main/scala/fraud/main/Domain.scala
+++ fraud-main/src/main/scala/fraud/main/Domain.scala
@@ -1,8 +1,26 @@
 package fraud.main
 
-case class Transaction(id : String , user : String, amount: String, timestamp : String)
+import org.apache.spark.mllib.linalg.Vectors
+
+
+case class Transaction(id: String, user: String, receiver: String, amount: String, timestamp: String)
+
 
 object Domain {
+  val receivers = Seq("Albert Hein", "E-Bay", "Power Company")
+  val receiverIds = Map(receivers(0) -> 0, receivers(1) -> 1, receivers(2) -> 2)
+
+  def features(t: Transaction) = Vectors.dense(receiverId(t), amountId(t))
+
+  def receiverId(t: Transaction): Int = receiverIds(t.receiver)
+
+  def amountId(t: Transaction): Int = {
+    val amount = t.amount.toDouble
+    if (amount < 0.0) throw new IllegalArgumentException(s"Amount can not be negative, amount = $amount")
+    if (amount < 1.00) 0
+    else if (amount < 100) 1
+    else 2
+  }
 
 }
 
