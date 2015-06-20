9f5a0973a5f96b084ab943b58996d26a73d8491e
Borys Biletskyy <borys.biletskyy@gmail.com>
2015-06-20 10:47:53 +0200

    Html generating methods extracted from trait in REST service

--- fraud-main/src/main/scala/fraud/main/RestServiceActor.scala
+++ fraud-main/src/main/scala/fraud/main/RestServiceActor.scala
@@ -13,66 +13,78 @@ import scala.xml.Elem
 /** Actor Service that  */
 class RestServiceActor(connector: ActorRef) extends Actor with RestService {
   def actorRefFactory = context
-
   def receive = runRoute(route)
-
   def communicate(t: Transaction) = connector ! t
-
   override def preStart() = println(s"Starting rest-service actor at ${context.self.path}")
-
   val session = Cluster.builder().addContactPoint("127.0.0.1").build().connect("fraud")
   def clean() = session.execute("TRUNCATE fraud_transactions")
   def selectFraudHTML() = session.execute("select * from fraud_transactions").iterator().map(t => <p>{ t.getString("transaction") }</p>)
+  def indexHtml(): Elem =
+    <html>
+      <body>
+        <h1>Transaction Fraud Detection Engine REST API</h1>
+        <a href="/fraud">View Detected Fraud Transactions</a>
+        <br/>
+        <a href="/transactions">View Random Transaction Examples</a>
+      </body>
+    </html>
 
+  def cleanHtml(): Elem = {
+    clean()
+    <html>
+      <body>
+        <h1>Fraud transactions have been cleaned</h1>
+        <a href="/fraud">View Detected Fraud Transactioons</a>
+      </body>
+    </html>
+  }
+  def fraudHtml(): Elem =
+    <html>
+      <body>
+        <h1>Detected Fraud Transactions:</h1>
+        <a href="/clean">Clean</a>
+        <p>{ selectFraudHTML() }</p>
+      </body>
+    </html>
 }
 
 /** This trait defines the routing */
 trait RestService extends HttpService {
   import TransactionJsonProtocol._
   def communicate(t: Transaction)
-  def clean(): ResultSet
-  def selectFraudHTML(): Iterator[Elem]
+  def indexHtml(): Elem
+  def cleanHtml(): Elem 
+  def fraudHtml(): Elem 
 
   val route =
     path("") {
       get {
         respondWithMediaType(`text/html`) {
           complete {
-            <html>
-              <body>
-                <h1>Transaction Fraud Detection Engine REST API</h1>
-                <a href="/fraud">View Detected Fraud Transactions</a>
-                <br/>
-                <a href="/transactions">View Random Transaction Examples</a>
-              </body>
-            </html>
+            indexHtml()
           }
         }
       }
     } ~ path("transaction") {
       post {
         entity(as[Transaction]) { transaction =>
-          communicate(transaction)
-          complete(transaction)
+          complete {
+            communicate(transaction)
+            transaction
+          }
         }
       }
     } ~ path("transactions") {
       get {
         respondWithMediaType(`application/json`) {
-          complete(randomTransactions(10))
+          complete { randomTransactions(10) }
         }
       }
     } ~ path("fraud") {
       get {
         respondWithMediaType(`text/html`) {
           complete {
-            <html>
-              <body>
-                <h1>Real Time Transaction Fraud Detection REST API</h1>
-                <a href="/clean">Clean</a>
-                <p>{ selectFraudHTML() }</p>
-              </body>
-            </html>
+            fraudHtml()
           }
         }
       }
@@ -80,14 +92,7 @@ trait RestService extends HttpService {
       get {
         respondWithMediaType(`text/html`) {
           complete {
-            clean()
-            <html>
-              <body>
-                <h1>Real Time Transaction Fraud Detection REST API</h1>
-                Fraud transactions are cleaned.
-                <a href="/fraud">View Detected Fraud Transactioons</a>
-              </body>
-            </html>
+            cleanHtml()
           }
         }
       }
